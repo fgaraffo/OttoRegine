aeebb268268225169e5b9c4e0095b629e8321663
fedeg <fedeg@Hp-Fede.lan>
2018-11-12 15:06:02 +0100

    soluzione-aula

---
+++ src/it/polito/tdp/regine/TestRegine.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.regine;
+
+public class TestRegine {
+
+	public static void main(String[] args) {
+		
+		Regine r = new Regine();
+		
+		System.out.println(r.posiziona(8));
+
+	}
+
+}
